618c0617a4044fc616406c0f34102baa43f09714
mathematicalninja <github@mathematics.ninja>
2025-10-24 01:42:16 +0100

    removed outdated comments

--- lua/floating/state/get_last_win.lua
+++ lua/floating/state/get_last_win.lua
@@ -1,6 +1,3 @@
---- usage: STATE:get_last() or
----
----  Float.get_last = function() return Float.state.get_last(Float.state) end
 --- @param STATE STATE
 local function Get_Last_Win(STATE)
     -- local index = STATE:get_last_index()
