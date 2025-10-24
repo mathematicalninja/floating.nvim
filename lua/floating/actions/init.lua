d75c743b64626df2b86279de9be3a37ebb222f2c
mathematicalninja <github@mathematics.ninja>
2025-10-24 01:24:18 +0100

    cleaned up Style

--- lua/floating/actions/init.lua
+++ lua/floating/actions/init.lua
@@ -2,14 +2,12 @@
 --- @field buffer ACTIONS.BUFFER
 --- @field draw ACTIONS.DRAW
 --- @field open ACTIONS.OPEN
---- @field style ACTIONS.STYLES
 
 --- @type ACTIONS
 local Actions = {
     buffer = require("floating.actions.buffer"),
     draw = require("floating.actions.draw"),
     open = require("floating.actions.open"),
-    style = require("floating.actions.style"),
 }
 
 return Actions
